d9df9a29f55b5cd2202393142d85dc3592649aee
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2025-03-04 21:07:48 -0500

    refactor styles in main_view.py

---
+++ src/styles.py
@@ -0,0 +1,7 @@
+# Diccionario con los estilos de fuente para la aplicación
+STYLES = {
+    'title': {'font': ('Helvetica', 16, 'bold')},
+    'subtitle': {'font': ('Helvetica', 14)},
+    'creator_title': {'font': ('Helvetica', 12, 'italic')},
+    'body': {'font': ('Helvetica', 10)}
+}
--- src/view/main_view.py
+++ src/view/main_view.py
@@ -2,17 +2,39 @@ import tkinter as tk
 from tkinter import ttk
 
 from src.config import CREATORS
+from src.styles import STYLES
 
 
 class WelcomeView:
 
     def __init__(self, controller=None):
         self.controller = controller
+        # Esta es la ventana principal de la aplicación
         self.root = tk.Tk()
+
         self.root.title("Simulador de Números Pseudoaleatorios")
         self.creators = CREATORS
         self.create_widgets()
         self.center_window()
+
+    def create_widgets(self):
+        # Este es el panel principal de la ventana, como JPanel en Java
+        self.frame = ttk.Frame(self.root, padding=20)
+        self.frame.pack(fill='both', expand=True)
+
+        label_title = self.create_label("Bienvenido al Simulador", 'title')
+        label_title.pack(pady=10)
+
+        # Subtítulo
+        label_subtitle = self.create_label("Simulador de Números Pseudoaleatorios", 'subtitle')
+        label_subtitle.pack(pady=5)
+
+        # Información de los creadores
+        self.create_creator_label()
+
+        # Botón "Usar Simulador"
+        self.btn_simulator = ttk.Button(self.frame, text="Usar Simulador", command=self.on_use_simulator)
+        self.btn_simulator.pack(pady=20)
 
     def center_window(self):
         self.root.update_idletasks()
@@ -26,29 +48,16 @@ class WelcomeView:
 
         self.root.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
 
-    def create_widgets(self):
-        self.frame = ttk.Frame(self.root, padding=20)
-        self.frame.pack(fill='both', expand=True)
-
-        self.label_title = ttk.Label(self.frame, text="Bienvenido al Simulador", font=("Helvetica", 16))
-        self.label_title.pack(pady=10)
-
-        # Subtítulo
-        self.label_subtitle = ttk.Label(self.frame, text="Simulador de Números Pseudoaleatorios",
-                                        font=("Helvetica", 12))
-        self.label_subtitle.pack(pady=5)
-
-        # Información de los creadores
-        self.create_creator_label()
-
-        # Botón "Usar Simulador"
-        self.btn_simulator = ttk.Button(self.frame, text="Usar Simulador", command=self.on_use_simulator)
-        self.btn_simulator.pack(pady=20)
-
     def create_creator_label(self):
-        ttk.Label(self.frame, text="Creado por:", font=("Helvetica", 12)).pack(pady=5)
+        label_creator = self.create_label("Creado por:", 'creator_title')
+        label_creator.pack(pady=5)
         for creator in self.creators:
-            ttk.Label(self.frame, text=creator, font=("Helvetica", 10)).pack(pady=2)
+            label_creator = self.create_label(creator, 'body')
+            label_creator.pack(pady=5)
+
+    def create_label(self, text, style):
+        imported_style = STYLES.get(style, {})
+        return ttk.Label(self.frame, text=text, **imported_style)
 
     def on_use_simulator(self):
         # Acción al pulsar el botón: aquí se podría cambiar a la vista del simulador
