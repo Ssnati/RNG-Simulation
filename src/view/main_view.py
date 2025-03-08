f80366b84bfa418a344f2039c3a45a24629d51c2
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2025-03-08 11:06:38 -0500

    refactorized view strucure class as a Frame

--- src/view/main_view.py
+++ src/view/main_view.py
@@ -1,20 +1,20 @@
+import tkinter as tk
 from tkinter import ttk
 
 from src.config import CREATORS
 from src.styles import STYLES
 
 
-class WelcomeView:
-    def __init__(self, root, controller=None):
+class WelcomeView(tk.Frame):
+    def __init__(self, root_window, controller=None):
+        super().__init__()
         self.controller = controller
-        self.root = root
-        self.frame = ttk.Frame(self.root)
+        self.root_window = root_window
+        self.frame_container = ttk.Frame(self.root_window)
         self.creators = CREATORS
         self.create_widgets()
 
     def create_widgets(self):
-        self.frame.pack(fill='both', expand=True, padx=20, pady=20)
-
         label_title = self.create_label("Bienvenido al Simulador", 'title')
         label_title.pack(pady=10)
 
@@ -24,7 +24,7 @@ class WelcomeView:
         self.create_creator_label()
 
         self.btn_simulator = ttk.Button(
-            self.frame,
+            self.frame_container,
             text="Usar Simulador",
             command=self.on_use_simulator
         )
@@ -39,14 +39,14 @@ class WelcomeView:
 
     def create_label(self, text, style):
         imported_style = STYLES.get(style, {})
-        return ttk.Label(self.frame, text=text, **imported_style)
+        return ttk.Label(self.frame_container, text=text, **imported_style)
 
     def on_use_simulator(self):
         if self.controller:
             self.controller.show_simulator()
 
     def destroy(self):
-        self.frame.destroy()
+        self.frame_container.destroy()
 
     def pack(self):
-        self.frame.pack(fill='both', expand=True)
+        self.frame_container.pack(fill='both', expand=True, padx=20, pady=20)
--- src/view/simulator_view.py
+++ src/view/simulator_view.py
@@ -1,27 +1,27 @@
+import tkinter as tk
 from tkinter import ttk
 
 from src.styles import STYLES
 
 
-class SimulatorView:
+class SimulatorView(tk.Frame):
 
-    def __init__(self, master, controller):
-        self.master = master
+    def __init__(self, root_window, controller):
+        super().__init__()
+        self.root_window = root_window
         self.controller = controller
-        self.frame = ttk.Frame(self.master)
+        self.frame_container = ttk.Frame(self.root_window)
         self.create_widgets()
 
     def create_widgets(self):
-        self.frame.pack(fill='both', expand=True, padx=20, pady=20)
-
         self.create_label("Simulador de Números Pseudoaleatorios", 'title').pack(pady=10)
 
     def create_label(self, text, style):
         imported_style = STYLES.get(style, {})
-        return ttk.Label(self.frame, text=text, **imported_style)
+        return ttk.Label(self.frame_container, text=text, **imported_style)
 
     def destroy(self):
-        self.frame.destroy()
+        self.frame_container.destroy()
 
     def pack(self):
-        self.frame.pack(fill='both', expand=True)
+        self.frame_container.pack(fill='both', expand=True, padx=20, pady=20)
